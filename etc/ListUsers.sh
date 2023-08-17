d312cb47e0247dc63fcb40f56110c8f7ba2dddd9
G.Ab <12986104+IRNCR@users.noreply.github.com>
2023-08-17 10:17:33 +0330

    Update ListUsers.sh

--- etc/ListUsers.sh
+++ etc/ListUsers.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Define font colors
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 clear
 
+# Get a list of all users with UID >= 1000
 allusers=$(awk -F: '$3>=1000 {print $1}' /etc/passwd | grep -v nobody)
-echo -e "${GREEN}$allusers${ENDCOLOR}"
 
-echo -e "\nPress Enter key to return to main menu"; read
+# Display the list of users
+echo -e "${GREEN}List of users with UID >= 1000:\n"
+echo "$allusers${ENDCOLOR}"
+
+# Return to panel
+echo -e "\nPress Enter key to return to the main menu"; read
 menu
